3e50fd49330943224071e00600257ce95445b8a5
McKee <zmckee@zagmail.gonzaga.edu>
2019-10-08 12:06:07 -0700

    Finished text watcher

--- TextWatcherFun/app/src/main/java/com/example/textwatcherfun/MainActivity.java
+++ TextWatcherFun/app/src/main/java/com/example/textwatcherfun/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.textwatcherfun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,8 +32,36 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     editT.setText("");
                     textV.setVisibility(View.INVISIBLE);
+                    Toast toast = Toast.makeText(MainActivity.this, "Please enter a name", Toast.LENGTH_SHORT);
+                    toast.show();
                 }
+            }
+        });
 
+        ((EditText) findViewById(R.id.editText)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView textV = findViewById(R.id.textView);
+                String theName = s.toString();
+                if(!theName.equals("")){
+                    textV.setText("Hello " + theName);
+                    textV.setVisibility(View.VISIBLE);
+                } else {
+                    textV.setVisibility(View.INVISIBLE);
+                    Toast toast = Toast.makeText(MainActivity.this, "Please enter a name", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
     }
